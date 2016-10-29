71d3fead54d4e3693d725b98fa7d1d468fd4b2d3
Justas <greibus.justinas@gmail.com>
2016-10-29 21:10:50 +0300

    Moved actions to the TOOLS category

--- plugins/md_issue_tracker_integration/src/main/java/trackerIntegrationManager/MainMenuConfigurator.java
+++ plugins/md_issue_tracker_integration/src/main/java/trackerIntegrationManager/MainMenuConfigurator.java
@@ -8,40 +8,26 @@ import com.nomagic.magicdraw.actions.MDActionsCategory;
 import com.nomagic.magicdraw.core.Application;
 import com.nomagic.magicdraw.core.Project;
 
-/**
- * Created by justinasg on 2016-08-21.
- */
 public class MainMenuConfigurator implements AMConfigurator
     {
 
-        String MENU ="Tracker Integration";
+        String PARENT_MENU ="TOOLS";
+        String MENU = "Tracker Integration";
 
-        /**
-         * Action will be added to manager.
-         */
         private NMAction action;
 
-        /**
-         * Creates configurator.
-         * @param action action to be added to main menu.
-         */
         public MainMenuConfigurator(NMAction action)
         {
             this.action = action;
         }
 
-        /**
-         * @see com.nomagic.actions.AMConfigurator#configure(ActionsManager)
-         *  Methods adds action to given manager Examples category.
-         */
         @Override
         public void configure(ActionsManager manager) {
-            // searching for Examples action category
 
-            ActionsCategory category = (ActionsCategory) manager.getActionFor(MENU);
+
+            ActionsCategory category = (ActionsCategory) manager.getCategory(PARENT_MENU);
 
             if (category == null) {
-                // creating new category
                 category = new MDActionsCategory(MENU, MENU);
                 category.setNested(true);
                 manager.addCategory(category);
