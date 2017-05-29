95f51eae869ab168546443ce71dfa6e696007ff9
jgreibus <greibus.justinas@gmail.com>
2017-05-29 21:39:46 +0300

    Adding action for TC selection (created method for collecting verified requirements)

--- plugins/md_issue_tracker_integration/src/main/java/lt/jgreibus/magicdraw/redmine/plugin/actions/AddTestCasesToIssues.java
+++ plugins/md_issue_tracker_integration/src/main/java/lt/jgreibus/magicdraw/redmine/plugin/actions/AddTestCasesToIssues.java
@@ -6,24 +6,18 @@ import com.nomagic.magicdraw.core.Project;
 import com.nomagic.magicdraw.ui.dialogs.selection.ElementSelectionDlg;
 import com.nomagic.magicdraw.uml.BaseElement;
 import com.nomagic.magicdraw.uml.ClassTypes;
-import com.nomagic.uml2.ext.jmi.helpers.ClassifierHelper;
-import com.nomagic.uml2.ext.jmi.helpers.ModelHelper;
 import com.nomagic.uml2.ext.jmi.helpers.StereotypesHelper;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Class;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.DirectedRelationship;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element;
-import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Package;
+import com.nomagic.uml2.ext.magicdraw.commonbehaviors.mdbasicbehaviors.Behavior;
 import com.nomagic.uml2.ext.magicdraw.mdprofiles.Profile;
 import com.nomagic.uml2.ext.magicdraw.mdprofiles.Stereotype;
 
 import java.awt.event.ActionEvent;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
 
-import static com.nomagic.magicdraw.persistence.xmi.i.el;
-import static lt.jgreibus.magicdraw.redmine.element.manager.ElementSelectionManager.createElementSelectionDialog;
 import static lt.jgreibus.magicdraw.redmine.element.manager.ElementSelectionManager.createStereotypedElementsSelectionDialog;
 
 /**
@@ -38,28 +32,39 @@ public class AddTestCasesToIssues extends MDAction{
         //List<Class> types = ClassTypes.getSubtypes(Class.class);
         List<java.lang.Class> types = ClassTypes.getSubtypes(Class.class);
         Project project = Application.getInstance().getProject();
-        Profile profile = StereotypesHelper.getProfile(project, "UML Testing Profile");
-        Stereotype stereotype = StereotypesHelper.getStereotype(project, "TestCase", profile);
+        Profile profile = StereotypesHelper.getProfile(project, "Softneta Medical Profile");
+        Stereotype stereotype = StereotypesHelper.getStereotype(project, "TC", profile);
 
         ElementSelectionDlg elementSelectionDlg = createStereotypedElementsSelectionDialog(types, stereotype);
-
         elementSelectionDlg.setVisible(true);
 
         if (elementSelectionDlg.isOkClicked()) {
-
             List<BaseElement> selectedElements = elementSelectionDlg.getSelectedElements();
-
-            for(BaseElement bel : selectedElements){
-
-                Element el = (Element) bel;
-
-                System.out.println(el.refGetValue("Verifies"));
-               // ModelHelper.
-              if(StereotypesHelper.hasStereotype((Element) el, stereotype)){
-
-              }
-            }
+            extractVerifiedRequirementsFromTC(selectedElements);
         }
     }
+
+    private static Collection<Element> extractVerifiedRequirementsFromTC(List<BaseElement> selectedElements) {
+        Collection<Element> req = null;
+        for(BaseElement bel : selectedElements){
+            Behavior el = (Behavior) bel;
+            Collection<DirectedRelationship> targets = el.get_directedRelationshipOfSource();
+            for (DirectedRelationship relationship : targets)
+            {
+                if(StereotypesHelper.hasStereotype(relationship, "Verify")){
+                    Collection<Element> target = relationship.getTarget();
+                    if(target != null && !target.isEmpty()){
+                        for(Element t : target)
+                            if(StereotypesHelper.hasStereotype(t, "Requirement")) req.add(t);
+                    }
+                }
+            }
+        }
+//        for(Element e : req){
+//            System.out.println(e.getHumanName());
+//        }
+        return req;
+    }
+
     //TBD
 }
